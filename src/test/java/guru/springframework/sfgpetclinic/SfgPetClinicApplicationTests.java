365bfb3c0c874b467b23fb1b5d2632c19f8daa99
Krzysztof Gurniak <kris1337@gmail.com>
2019-11-01 18:35:48 +0100

    initial commit with starter project from spring initalizer

---
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
