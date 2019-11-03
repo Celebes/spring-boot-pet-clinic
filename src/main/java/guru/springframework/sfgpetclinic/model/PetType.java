a5251af7d73691a656b68e86d648771698940876
Krzysztof Gurniak <kris1337@gmail.com>
2019-11-03 15:42:03 +0100

    basic model POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
