c01303db494219b454514548214dfd6c72d0ab19
Krzysztof Gurniak <kris1337@gmail.com>
2019-11-03 17:40:39 +0100

    multi-module maven project with spring boot

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # spring-boot-pet-clinic
 Pet Clinic info: https://speakerdeck.com/michaelisvy/spring-petclinic-sample-application
+
+Based on Udemy course: https://www.udemy.com/course/spring-framework-5-beginner-to-guru
+
+## run using Spring Boot plugin
+
+Go to **`/pet-clinic-web`** directory and execute command **`mvn clean spring-boot:run`**
+
+## run manually
+
+Execute **`mvn clean install -U`** in root directory and then run **`java -jar .\pet-clinic-web\target\pet-clinic-web-0.0.1-SNAPSHOT.jar`**.
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+        Person p = new Person();
+        p.setFirstName("Kazik");
+        System.out.println(p.getFirstName());
     }
 
 }
